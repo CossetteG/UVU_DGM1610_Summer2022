9e554fabe0dad5a5c179ea1e64c1fe878f8eba94
Cossette <10838257@my.uvu.edu>
2022-06-21 10:57:18 -0600

    added a Debug.Log to a script

--- New Unity Project/Assets/BehavioiurScript1.cs
+++ New Unity Project/Assets/BehavioiurScript1.cs
@@ -7,7 +7,7 @@ public class BehavioiurScript1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start Game");
     }
 
     // Update is called once per frame
