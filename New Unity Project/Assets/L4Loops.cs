c3482c3793a5e547b1217767b8aa9b3dd4ddda72
Cossette <10838257@my.uvu.edu>
2022-06-26 15:48:09 -0600

    L 5: Scope and Access

---
+++ New Unity Project/Assets/L4Loops.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L4Loops : MonoBehaviour
+{
+    public int numEnemies = 3;
+    public int cupsInTheSink = 4;
+    
+    // Start is called before the first frame update
+    //4 different loops
+    //not tested
+    void Start()
+    {
+        for (int i = 0; i < numEnemies; i++)
+        {
+            Debug.Log("Creating enemy number " + i);
+        }
+        
+        while(cupsInTheSink > 0)
+        {
+             Debug.Log ("I've washed a cup!");
+             cupsInTheSink--;
+        }
+        
+        bool shouldContinue = false;
+
+        do
+        {
+            print ("Hello World");
+            
+        }while(shouldContinue == true);
+        
+        string[] pencils = new string[3];
+        
+        pencils[0] = "Red Pencil";
+        pencils[1] = "Green Pencil";
+        pencils[2] = "Yellow Pencil";
+
+        foreach (string item in pencils)
+        {
+            print (item);
+        }
+
+    }   
+}
---
+++ New Unity Project/Assets/L5ScopeAndAccess.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L5ScopeAndAccess : MonoBehaviour
+{
+    public int alpha = 5;
+    //this int can be used in any class and can be edited in the Inspector
+    private int beta = 1;
+    private int gamma = 3;
+    //these ints are only for use within this class
+    
+    //Editing variable is initialization>declaration>inspector edit>function declaration>inspector edit after Start (resets)
+
+    private AnotherClass myOtherClass;
+    
+    // Start is called before the first frame update
+    //Not tested
+    void Start()
+    {
+        alpha = 20;
+
+        myOtherClass = new AnotherClass();
+        myOtherClass.FruitMachine(alpha, myOtherClass.apples);
+    }
+
+    void Example (int pens, int crayons)
+    {
+        int answer;
+        answer = pens + crayons + alpha;
+        Debug.Log(answer);
+    }
+    void Update()
+    {
+        bool Continue = false;
+
+        do
+        {
+            Debug.Log("Alpha is set to " + alpha);
+            Debug.Log("Beta is set to " + beta);
+            Debug.Log("Gamma is set to " + gamma);
+        } while (Continue == true);
+    }
+}
