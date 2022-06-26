ac0e11f9d04bc7bafcc67478d066746b65613062
Cossette <10838257@my.uvu.edu>
2022-06-26 14:19:22 -0600

    L2 and L3

---
+++ New Unity Project/Assets/L2Double.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L2Double : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Not tested
+
+    public int myInt = 5; 
+    
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+    int MultiplyByTwo(int number)
+    //"int" here tells this function to return an integer
+    {
+        int result;
+        result = number * 2;
+        return result;
+    }
+}
---
+++ New Unity Project/Assets/L3CoffeeTester.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L3CoffeeTester : MonoBehaviour
+{
+    public float coffeeTemperature = 85.0f;
+    public float hotLimitTemperature = 70.0f;
+    public float coldLimitTemperature = 40.0f;
+        
+
+    // Update is called once per frame
+    //not tested
+    //I skipped the syntax lesson bc I know it already. stoopid
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TemperatureTest();
+        }
+
+        coffeeTemperature -= Time.deltaTime * 5f;
+    }
+
+    void TemperatureTest()
+    {
+        //too hot
+        if (coffeeTemperature > hotLimitTemperature)
+        {
+            print("Coffee is too hot");
+        }
+        else if (coffeeTemperature < coldLimitTemperature)
+        {
+            print("Coffee is too cold");
+        }
+        else
+        {
+            print("Coffee is just right");
+        }
+    }
+}
