a4f53ac2af3f60afb62ed2a73276ea60d40ffd43
Cossette <10838257@my.uvu.edu>
2022-07-03 00:32:27 -0600

    ScriptableObjects Lab

---
+++ New Unity Project/Assets/ScriptableObjExample.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class ScriptableObjExample : ScriptableObject
+{
+    public UnityEvent onZeroEvent;
+
+    public float value1;
+
+    public void UpdateValue(float num)
+    {
+        value1 += num; 
+    }
+
+    public void ReplaceValue(float num)
+    {
+        value1 = num;
+    }
+
+    public void DisplayImage(Image img)
+    {
+        if (value1 <= 0)
+            onZeroEvent.Invoke();
+        else if (value1 >= 1)
+            value1 = 1;
+
+        img.fillAmount = value1;
+    }
+
+    public void DisplayNumber(Text txt)
+    {
+        txt.text = value1.ToString();
+    }
+}
