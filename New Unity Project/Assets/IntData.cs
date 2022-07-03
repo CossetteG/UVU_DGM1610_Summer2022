b4707635fdb0e185793f665cda994dc9458d776b
Cossette <10838257@my.uvu.edu>
2022-07-03 13:30:35 -0600

    2nd PowerUp
    the text update did not work

---
+++ New Unity Project/Assets/IntData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class IntData : ScriptableObject
+{
+    public int numberr;
+
+    public void UpdateNumber(int numb)
+    {
+        numberr += numb;
+    }
+}
---
+++ New Unity Project/Assets/TextPoints.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(Text))]
+public class TextPoints : MonoBehaviour
+{
+    public Text ptText;
+    public IntData ptInt;
+    
+    private void Start()
+    {
+        ptText = GetComponent<Text>();
+        UpdateText();
+    }
+
+    public void UpdateText()
+    {
+        ptText.text = ptInt.numberr.ToString();
+    }
+}
