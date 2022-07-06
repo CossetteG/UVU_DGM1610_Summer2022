64dd31447c4a565dba09f08824f635b35520b583
Cossette <10838257@my.uvu.edu>
2022-07-06 00:45:26 -0600

    idk. I mightve broke somethin

---
+++ New Unity Project/Assets/MouseEnterEventBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEnterEventBehaviour : MonoBehaviour
+{
+    public UnityEvent mouseEnterEvent;
+
+    private void OnMouseEnter(Collider other)
+    {
+        mouseEnterEvent.Invoke();
+    }
+}
--- New Unity Project/Assets/WordDataBehaviour.cs
+++ New Unity Project/Assets/WordDataBehaviour.cs
@@ -6,11 +6,11 @@ public class WordDataBehaviour : MonoBehaviour
 {
     public Text werds;
     public TextData dataObjj;
+    public TextData startObj;
     
     private void Start()
     {
         werds = GetComponent<Text>();
-       
     }
 
     public void Update()
